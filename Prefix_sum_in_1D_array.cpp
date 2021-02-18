b4e6b09853d9ec8eddfcb9c6e0373e41eeb7b4ef
Shanto <kamrul0628@gmail.com>
2021-02-19 00:59:42 +0600

    .

--- Prefix_sum_in_1D_array.cpp
+++ Prefix_sum_in_1D_array.cpp
@@ -1,1 +1,31 @@
-#include <bits/stdc++.h>using namespace std;/*    Shanto ~~ A peaceful soul*/// prefix sum in 1d arrayconst int N = 1e5+10;int a[N];int pf[N];int main(){	int n;	cin >> n;	for (int i = 1; i <= n; ++i)	{		cin >> a[i];		pf[i] = pf[i-1] + a[i];	}	int q;	cin>>q;	while(q--){		int l,r;		cin>>l>>r;				cout<<pf[r] - pf[l-1]<<endl;	}}
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/*
+    Shanto ~~ A peaceful soul
+*/
+
+// prefix sum in 1d array
+const int N = 1e5+10;
+int a[N];
+int pf[N];
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> a[i];
+		pf[i] = pf[i-1] + a[i];
+	}
+	int q;
+	cin>>q;
+	while(q--){
+		int l,r;
+		cin>>l>>r;
+		
+		cout<<pf[r] - pf[l-1]<<endl;
+	}
+
+}
